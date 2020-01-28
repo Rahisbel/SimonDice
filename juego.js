7805f1947b0d4778b95578de6e0fa33de9325f5e
Rahisbel <ryhn236@gmail.com>
2020-01-28 11:21:43 -0400

    iluminando la secuencia de colores

--- juego.js
+++ juego.js
@@ -13,6 +13,7 @@ class Juego{
 	constructor(){
 		this.inicializar()
 		this.generarSecuencia()
+		this.siguienteNivel()
 	}
 
 	inicializar(){
@@ -29,7 +30,42 @@ class Juego{
 	generarSecuencia(){
 		this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random()*4))
 	}
- }
+
+	siguienteNivel(){
+		this.iluminarSecuencia()
+	}
+
+	transformarNumeroAColor(numero){
+		switch(numero){
+			case 0 : 
+				return 'azul'
+			case 1 : 
+				return 'violeta'
+			case 2 : 
+				return 'rosa'
+			case 3 : 
+				return 'verde'
+		}
+	}
+
+	iluminarSecuencia(){
+	// en los ciclos for se recomienda usar let
+		for (let i = 0; i < this.nivel; i++) {
+			const color = this.transformarNumeroAColor(this.secuencia[i])
+			setTimeout(()=> this.iluminarColor(color),1000*i)
+		}
+	} 
+
+	iluminarColor(color){
+		this.colores[color] .classList.add('claro')
+		setTimeout(() => this.apagarColor(color),350)
+	}
+
+	apagarColor(color){
+		this.colores[color].classList.remove('claro')
+	}
+}
+
 
 function empezarJuego(){
 	window.juego = new Juego()
